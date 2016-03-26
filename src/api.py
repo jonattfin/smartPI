e43c6e649e95466e3e15bc0e8aefef0dd295e156
jonattfin <jonattfin@gmail.com>
2016-03-27 00:34:16 +0200

    fix

---
+++ src/api.py
@@ -0,0 +1,39 @@
+import json
+import requests
+
+class CommonApi(object):
+
+    def __init__(self, url, table_name):
+        self.url = url
+        self.table_name = table_name
+
+    def write(self, value):
+        try:
+            params = {'value': value, 'table_name': self.table_name}
+            response = requests.post(self.url, data=params)
+        except Exception as e:
+            print("Error while writing to API")
+
+class LedApi(object):
+
+    def __init__(self, url):
+        self.url = url
+
+    def write(self, color):
+        try:
+            params = {'color': color}
+            response = requests.post(self.url, data=params)
+        except Exception as e:
+            print("Error while writing led to API")
+
+if __name__ == '__main__':
+    print('writing temperatures')
+
+    common_api = CommonApi("temperatures");
+    common_api.write(1234);
+
+    led_api = LedApi();
+    led_api.write(1);
+    led_api.write(2);
+    led_api.write(3);
+    led_api.write(4);
--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from ruleEngine import RuleEngine
 from spInterface import SpInterface
 from display import Display
+from api import CommonApi, LedApi;
 from rules import HumidityRule, TemperatureRule, LuminosityRule, PsutilRule, ButtonRule
 
 from collections import namedtuple
@@ -10,17 +11,18 @@ def main():
     sp_interface = SpInterface()
     Settings = namedtuple('Settings', 'periodicity sp_interface pin_no number_of_reads time_between_reads')
 
-    temp_rule = TemperatureRule(Settings(10*60, sp_interface, 0, 5, 2), [Display('temperature')])
-    humidity_rule = HumidityRule(Settings(10*60, sp_interface, 1, 5, 2), [Display('humidity')])
-    luminosity_rule = LuminosityRule(Settings(5*60, sp_interface, 2, 5, 2), [Display('luminosity')])
-    psutil_rule = PsutilRule(1*60, [Display('cpu'), Display('memory')])
+    temp_rule = TemperatureRule(Settings(10*60, sp_interface, 0, 5, 2), [Display('temperature'), CommonApi('temperatures')])
+    humidity_rule = HumidityRule(Settings(15*60, sp_interface, 1, 5, 2), [Display('humidity'), CommonApi('humidities')])
+    luminosity_rule = LuminosityRule(Settings(7*60, sp_interface, 2, 5, 2), [Display('luminosity'), CommonApi('luminosities')])
+    # psutil_rule = PsutilRule(4*60, [Display('cpu'), Display('memory')])
 
     led_displays = [Display('blue_led'), Display('red_led'), Display('green_led'), Display('yellow_led')]
-    button_rule = ButtonRule(Settings(3, sp_interface, 3, 0, 0), led_displays)
+    led_apis = [LedApi(color=2), LedApi(color=1), LedApi(color=4), LedApi(color=3)]
+    button_rule = ButtonRule(Settings(1*60, sp_interface, 3, 0, 0), led_displays + led_apis)
 
     # add the rules
     rule_engine = RuleEngine()
-    rule_engine.add_many([button_rule, psutil_rule, temp_rule, humidity_rule, luminosity_rule])
+    rule_engine.add_many([button_rule, temp_rule, humidity_rule, luminosity_rule])
 
     # fire up the engine
     rule_engine.execute()
